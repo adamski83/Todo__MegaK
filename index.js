35bc6420062699b40e9c2b4fa6b2f1d3a14685db
adamski83 <66271771+adamski83@users.noreply.github.com>
2022-06-26 16:06:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const { engine } = require("express-handlebars");
+const mongoose = require("mongoose");
+const Todo = require("./models/todo");
+const port = 3000;
+
+// const dbUrl = "mongodb://localhost:27017/tododb";
+const dbUrl =
+	"mongodb+srv://admin:12345@cluster0.ykrfi.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// app.engine("handlebars", engine());
+// app.set("view engine", "handlebars");
+// app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+	const searchRes = await Todo.find({}).then((result) => {
+		res.render("index", { data: result });
+		console.log(result);
+	});
+});
+
+app.post("/", (req, res) => {
+	const todo = new Todo({
+		todo: req.body.todoValue,
+	});
+
+	todo.save().then((result) => {
+		res.redirect("/");
+	});
+});
+app.delete("/:id", async (req, res) => {
+	Todo.findByIdAndDelete(req.params.id).then((result) => console.log(result));
+});
+
+app.listen(port, () =>
+	console.log(`Application running on potra ${port}: http://localhost:3000`)
+);
